ee5ece529a3ae545cfaf45c4e2101f22f4a50577
duke <pipi770@gmail.com>
2020-11-26 00:38:49 +0900

    Create Customer 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system</h2>
+const customers = [
+{
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'홍길동',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name':'춘향이',
+  'birthday':'880101',
+  'gender':'여자',
+  'job':'직장인'
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name':'방자',
+  'birthday':'930303',
+  'gender':'남자',
+  'job':'초등생'
+}
+]
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />);
+          })
+        }        
+      </div>
       
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
